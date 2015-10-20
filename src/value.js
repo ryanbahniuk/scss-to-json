af21d970a604a64a8ebf7be70586dd85bc82f611
Ryan Bahniuk <ryan@wealthfront.com>
2015-10-20 07:52:45 -0700

    making failing tests pass with inline comments

--- src/value.js
+++ src/value.js
@@ -6,14 +6,29 @@ function removeFlags(value) {
   return value.replace(/\!\w+/g, '');
 }
 
+function removeInlineComments(value) {
+  var transformedValue = value;
+  var commentIndex = value.indexOf('//');
+
+  if (commentIndex > -1) {
+    transformedValue = transformedValue.substring(0, commentIndex - 1);
+  }
+
+  return transformedValue;
+}
+
+function transforms(value) {
+  return removeInlineComments(removeFlags(value));
+}
+
 function Value(scssString) {
   this._parse(scssString);
 }
 
 Value.prototype = {
   _parse: function(scssString) {
-    var deflagged = removeFlags(scssString);
-    var compiled = compile.fromString(deflagged);
+    var transformed = transforms(scssString);
+    var compiled = compile.fromString(transformed);
     this.value = compiled.trim();
   }
 };
